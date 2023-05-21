393fd3a55b76a55d4a3441cdbb9275843fa8f5af
Prachi <p9458722_gmail.com>
2023-05-21 10:54:22 +0530

    a

--- app.py
+++ app.py
@@ -1,5 +1,29 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, request, flash
+import numpy as np
+import joblib
+
+
 app = Flask(__name__)
+app.secret_key = "09klsa asdjk"
+model_names = [
+    
+]
+# load models
+def load_models():
+    models = []
+    for model in model_names:
+        models.append(joblib.load(f'{model}.pkl'))
+    return models
+
+# predict function
+def predict(data):
+    models = load_models()
+    results = []
+    for model in models:
+        results.append(model.predict(data)[0])  
+    # return most frequent prediction
+    most_frequent = np.bincount(results).argmax()
+    return most_frequent
 
 
 @app.route('/')
